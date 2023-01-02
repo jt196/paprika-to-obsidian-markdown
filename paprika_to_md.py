9908f549761b755ecc53d3e7e3d840365735f948
Quinn McHugh <qpmchugh@gmail.com>
2023-01-02 16:58:16 -0500

    Define encoding

--- paprika_to_md.py
+++ paprika_to_md.py
@@ -62,7 +62,7 @@ def json_manipulate(sourceFolder, targetFolder, resourceFolder, template):
         for filename in files:
             if re.search(r'\.json$', filename):
                 p = Path(sourceFolder + filename)
-                with open(p) as json_file:
+                with open(p, encoding="utf-8") as json_file:
                     d = {}
                     recipe_data = json.load(json_file)
                     # assign variable, split according to line breaks, remove any empty entries
@@ -214,7 +214,7 @@ def json_manipulate(sourceFolder, targetFolder, resourceFolder, template):
                     with open(template_file, "r") as f:
                         src = Template(f.read())
                         result = src.substitute(d)
-                        g = open(targetFolder + recipe_name + ".md", "w")
+                        g = open(targetFolder + recipe_name + ".md", "w", encoding="utf-8")
                         g.write(result)
                         g.close()
 
